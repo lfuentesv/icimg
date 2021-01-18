e82142c2651eb5178f1728cea1c56312cc3a9424
Leonardo <leonardo.fuentes@gmail.com>
2021-01-18 20:58:49 -0300

    Agrega repositorios vacios

---
+++ ICIMG/src/main/java/cl/lfuentes/icimg/dao/EjeRepository.java
@@ -0,0 +1,9 @@
+package cl.lfuentes.icimg.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.lfuentes.icimg.entityTo.Eje;
+
+public interface EjeRepository extends JpaRepository<Eje, String>{
+
+}
