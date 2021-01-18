286f44e45e351a5ecaa9ea5a4b3765ad3cb62b98
Leonardo <leonardo.fuentes@gmail.com>
2021-01-18 16:42:43 -0300

    Agrega estructura base

---
+++ ICIMG/src/test/java/cl/lfuentes/icimg/IcimgApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.lfuentes.icimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcimgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
