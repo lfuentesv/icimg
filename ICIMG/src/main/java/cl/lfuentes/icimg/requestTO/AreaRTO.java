50155734a1d5ee25cc92d4e94108bcb222ad734d
Leonardo <leonardo.fuentes@gmail.com>
2021-01-20 02:09:34 -0300

    Agrega tipo especial de request para el àrea

---
+++ ICIMG/src/main/java/cl/lfuentes/icimg/requestTO/AreaRTO.java
@@ -0,0 +1,32 @@
+package cl.lfuentes.icimg.requestTO;
+
+public class AreaRTO {
+
+	private String codigo;
+	private String nombre;
+	private String eje;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEje() {
+		return eje;
+	}
+	public void setEje(String eje) {
+		this.eje = eje;
+	}
+	
+	public AreaRTO() {
+
+	}
+
+}
