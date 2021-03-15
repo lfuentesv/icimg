872e6f655aa94b79aefcc627e8412f75f13afd71
Leonardo Fuentes <leonardo.fuentes@globallogic.com>
2021-03-15 09:26:37 -0300

    WIP: Agrega metodo put para recibir cambios en area

--- ICIMG/src/main/java/cl/lfuentes/icimg/api/AreasApi.java
+++ ICIMG/src/main/java/cl/lfuentes/icimg/api/AreasApi.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,13 @@ public class AreasApi {
 		Optional<Area> area = servicio.buscar(id);
 		
 		return ResponseEntity.ok(area);
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<Area> update(@Valid @RequestBody AreaRTO area ,@PathVariable(value = "id") String id){
+				
+		Area areaRespuesta = servicio.crear(area);
+		
+		return new ResponseEntity <>(areaRespuesta,HttpStatus.CREATED );
 	}
 	
 }
